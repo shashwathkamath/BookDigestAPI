15ce02d139b475861dc5dd83b1648c4bc8f563ad
Shashwath Kamath <kamathsh91@gmail.com>
2024-03-13 21:27:34 -0400

    data from db

---
+++ src/routes/bookRoutes.ts
@@ -0,0 +1,6 @@
+// import express from 'express';
+// import bookController from '../controllers/bookController';
+
+// const router = express.Router();
+// router.get('/', bookController.getAllBooks);
+// export default router;
