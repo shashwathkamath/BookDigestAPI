d62d936cf0f1a472c869d3848faf94b5ca06aa50
Shashwath Kamath <kamathsh91@gmail.com>
2024-07-26 22:19:13 -0400

    added get call for genres

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -1,8 +1,10 @@
 import express, { Router } from 'express';
 import { createBook, getAllBooks } from '../controllers/bookController';
+import { getAllGenres } from './genreRoutes';
 
 const router: Router = express.Router();
 router.get('/books', getAllBooks);
 router.post('/createBook', createBook);
+router.get('/genres', getAllGenres);
 
 export default router;
