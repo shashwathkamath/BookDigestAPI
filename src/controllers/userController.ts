059fe4ccbcdad5e1d6aeee7be277661c437b88a0
Shashwath Kamath <kamathsh91@gmail.com>
2024-12-22 19:38:38 -0500

    initial route for user done

---
+++ src/controllers/userController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+import User from '../models/User';
+
+export const createUser = async (req: Request, res: Response) => {
+    try {
+        const { id, name, email, photo, givenName, familyName } = req.body;
+
+        // Validate required fields
+        if (!email || !id) {
+            return res.status(400).json({ message: 'Email and ID are required' });
+        }
+
+        // Check if user already exists
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.status(409).json({ message: 'User already exists' });
+        }
+
+        // Create a new user
+        user = await User.create({
+            id,
+            name,
+            email,
+            profilePicUrl: photo,
+            givenName,
+            familyName,
+        });
+
+        return res.status(201).json({ message: 'User created successfully', user });
+    } catch (error) {
+        console.error('Error creating user:', error);
+        res.status(500).json({ message: 'Internal server error', error: error });
+    }
+};
